e13c5632d06d81b625348bd61dc7052eb607c515
Camila Franca <camila.franca@kellogg.northwestern.edu>
2023-05-17 17:29:12 +0000

    Places controller and index

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,39 @@
 class PlacesController < ApplicationController
+
+  def index
+    # find all Place rows
+    @places = Place.all
+    # render places/index view
+  end
+
+  def show
+    # find a Place
+    @place = Place.find_by({"id" => params["id"]})
+    # render places/show view with details about Place
+    # render :template => "places/show"
+  end
+
+  def new
+    @place = Place.new
+    # render view with new Place form
+  end
+  
+  def create
+    # start with a new Place
+    @place = Place.new
+
+    puts params["place"]
+
+    # assign user-entered form data to Place's columns
+    @place["name"] = params["place"]["name"]
+    @place["created_at"] = params["place"]["created_at"]
+    @place["updated_at"] = params["place"]["updated_at"]
+
+    # save Place row
+    @place.save
+
+    # redirect user
+    redirect_to "/places"
+  end
+
 end
